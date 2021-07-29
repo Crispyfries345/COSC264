ec98e5479d2f02d2bc89f03d72602c1d2882130a
Bailey Lissington <lissington4@gmail.com>
2021-07-30 09:14:09 +1200

    gets ip4v address from dns lookup

--- assignment_1/client.py
+++ assignment_1/client.py
@@ -1,8 +1,23 @@
 from argparse import ArgumentParser, ArgumentTypeError
 import socket
+import sys
 
 PORT_MIN: int = 1024
-PORT_MAX: int = 6400
+PORT_MAX: int = 64_000
+SOCKADDR_I: int = 4  # index of sockaddr returned by getaddrinfo
+
+
+def get_ipv4_addr(addr: str, port: int) -> str:
+    """Gets an IPv4 address for a given address and port,
+    performing a DNS lookup if address is a hostname"""
+    try:
+        ipv4_addr: str = socket.getaddrinfo(
+            addr, port, family=socket.AF_INET, type=socket.SOCK_STREAM
+        )[0][SOCKADDR_I][0]
+    except socket.gaierror as exc:
+        print(exc)
+        sys.exit(1)
+    return ipv4_addr
 
 
 def valid_port(port: str) -> int:
@@ -21,6 +36,10 @@ def main():
     parser.add_argument("filename", help="name of file to be retrieved from server")
 
     args = parser.parse_args()
+    port: int = args.port
+    ipv4_addr: str = get_ipv4_addr(args.address, port)
+    sockfd: socket.socket = socket.socket()
+    sockfd.connect((ipv4_addr, port))   
 
 
 if __name__ == "__main__":
